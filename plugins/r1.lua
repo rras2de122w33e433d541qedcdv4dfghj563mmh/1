40945179dc8e3a43756eb1a8d12f06191d63e5d5
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-04-24 10:43:01 -0700

    Update r1.lua

--- plugins/r1.lua
+++ plugins/r1.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_document(get_receiver(msg), "./stickers/turbo.webp", ok_cb, false)
+send_document(get_receiver(msg), "./rank/turbo.webp", ok_cb, false)
 end
 
 return {
