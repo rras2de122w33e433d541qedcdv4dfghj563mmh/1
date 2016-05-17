e2b28de3cb88f85606a1a390c18ff7cb5b6be7c9
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-05-17 01:57:21 -0400

    Update short.link.lua

--- plugins/short.link.lua
+++ plugins/short.link.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
-  local sina = URL.escape(matches[1])
-  url = "https://api-ssl.bitly.com/v3/shorten?access_token=f2d0b4eabb524aaaf22fbc51ca620ae0fa16753d&longUrl="..sina
+  local reza = URL.escape(matches[1])
+  url = "https://api-ssl.bitly.com/v3/shorten?access_token=f2d0b4eabb524aaaf22fbc51ca620ae0fa16753d&longUrl="..reza
   jstr, res = https.request(url)
   jdat = JSON.decode(jstr)
   if jdat.message then
-    return '???? ????? ??? \n___________\n\n'..jdat.message
+    return 'لینک کوتاه شده \n___________\n\n'..jdat.message
   else
-    return "???? ????? ???: \n___________\n"..jdat.data.url
+    return "لینک کوتاه شده: \n___________\n"..jdat.data.url
     end
   end
 
