4202474c72ed93dee1958de58e44e66287cc808f
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-05-19 11:51:53 -0400

    Update mean.lua

--- plugins/mean.lua
+++ plugins/mean.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
   local data = json:decode(htp)
-	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n\n@Turbo BoT'
+	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n\nTurbo BoT'
 end
 return {
   patterns = {
