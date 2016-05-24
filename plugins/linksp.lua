0fa733432580ec4b18d16fe78d1834317db46b04
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-05-24 04:39:57 -0400

    Update linksp.lua

--- plugins/linksp.lua
+++ plugins/linksp.lua
@@ -1,12 +1,12 @@
 do
 function run(msg, matches)  
 local data = load_data(_config.moderation.data)     
-local group_link = data[tostring(1065958052)]['settings']['set_link'] 
+local group_link = data[tostring(120541788)]['settings']['set_link'] 
 if not group_link then                                          
 return ''                                
 end                                        
 local text = "لینک ساپورت ربات توربو:\n"..group_link 
-send_large_msg('channel#id'..msg.to.id, text.."\n",ok_cb,false)      
+send_large_msg('chat#id'..msg.to.id, text.."\n",ok_cb,false)      
 end                
 return {               
 patterns = {
